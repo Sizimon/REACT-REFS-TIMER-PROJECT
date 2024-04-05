43846fcf1c0ff30d52c0dc3a3f6f43e890fb3f50
Szymon Samus <siizmonsus@gmail.com>
2024-04-05 19:33:01 +0200

    Add: Integrate refs to handle the timer challenge components. Dynamically output the game windows depending on whether the timer has been started or not.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,11 +7,12 @@ export default function Player() {
 
   function handleClick() {
     setEnteredName(playerName.current.value);
+    playerName.current.value = "";
   }
 
   return (
     <section id="player">
-      <h2>Welcome {enteredName ?? "unknown entity"}</h2>
+      <h2>Welcome, {enteredName ?? "unknown entity"}.</h2>
       <p>
         <input
          ref={playerName} 
